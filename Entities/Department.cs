e76edb833acbeb588bc00c9343328c90b77bdea9
Wesllen Felipe Langaro Raiser da Cruz <129122783+wesllenfe@users.noreply.github.com>
2024-04-06 10:21:48 -0300

    First commit

---
+++ Entities/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace exercicio_funcionario.Entities
+{
+    internal class Department
+    {
+        public string Name { get; set; }
+
+        public Department() { }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,9 @@
+﻿namespace exercicio_funcionario.Entities.Enums
+{
+    enum WorkerLevel : int
+    {
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2
+    }
+}
